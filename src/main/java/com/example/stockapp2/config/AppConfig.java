ce5b68a76fe646928120b33e1d30d235fdf1ede7
Olugbodi Johnny <olugbodijohnny@live.com>
2023-03-28 14:55:21 +0100

    testing auto deploy to AWS EC2 instance

--- src/main/java/com/example/stockapp2/config/AppConfig.java
+++ src/main/java/com/example/stockapp2/config/AppConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -117,5 +118,10 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate () {
         return new RestTemplate();
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
